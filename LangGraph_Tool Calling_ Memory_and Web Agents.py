51ec951197f704bfb3d7854f4ef429e4816b41a0
Mai Mohamed <179390147+MaiM0hamed@users.noreply.github.com>
2026-02-12 02:26:19 +0200

    Refactor LangGraph tool with SQLite memory integration

--- LangGraph_Tool Calling_ Memory_and Web Agents.py
+++ LangGraph_Tool Calling_ Memory_and Web Agents.py
@@ -10,7 +10,12 @@ import os
 from langchain_openai import ChatOpenAI
 from typing import TypedDict
 
+import sqlite3
+from langgraph.checkpoint.sqlite import SqliteSaver
 
+db_path = "memory.db"
+conn = sqlite3.connect(db_path, check_same_thread=False)
+sql_memory = SqliteSaver(conn)
 from langchain_community.utilities import GoogleSearchAPIWrapper
 from langchain_community.tools import tool
 
@@ -26,3 +31,27 @@ from langchain.agents import tool
 def google_search(query: str) -> str:
     """Perform a Google search and return the results."""
     return search.run(query)
+
+# Steps 1 and 2
+graph_builder = StateGraph(State)
+
+
+# Step 3
+llm = ChatOpenAI(model="gpt-4o-mini")
+llm_with_tools = llm.bind_tools(tools)
+
+def chatbot(state: State):
+    print(state)
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("tools", ToolNode(tools=tools))
+
+# Step 4
+graph_builder.add_conditional_edges( "chatbot", tools_condition, "tools")
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
+
+# Step 5
+graph = graph_builder.compile(checkpointer=sql_memory)
+display(Image(graph.get_graph().draw_mermaid_png()))
